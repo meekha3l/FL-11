cf21ffd62ad06353eaf99ab0a458c9835f915852
Mykhailo Petruniv <mykhaylo92ua@gmail.com>
2019-07-07 16:05:26 +0300

    small changes (const)

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -5,8 +5,10 @@ let b2 = +prompt('Enter b2', 0);
 let c1 = +prompt('Enter c1', 0);
 let c2 = +prompt('Enter c2', 0);
 
-let midDistance1 = (a1+b1) / 2;
-let midDistance2 = (a2+b2) / 2;
+const DIV_BY_HALF = 2
+
+let midDistance1 = (a1+b1) / DIV_BY_HALF;
+let midDistance2 = (a2+b2) / DIV_BY_HALF;
 
 if (midDistance1 === c1 && midDistance2 === c2) {
     console.log(true);
