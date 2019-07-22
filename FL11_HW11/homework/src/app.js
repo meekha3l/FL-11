fca17f30bd45b8578bb68ebafff7ff1fc8822e51
Mykhailo Petruniv <mykhaylo92ua@gmail.com>
2019-07-22 22:43:28 +0300

    added editItem(), delItem(), listenerItems(), changed addItem(), form fix

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -8,21 +8,12 @@ let itemNum = 0;
 let itemsAmount = [];
 const MAX_ITEMS = 10;
 
-console.log(form);
-
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener(`click`, () => {
     addItem(addField.value);
     addField.value = '';
 }, false);
 
-form.addEventListener(`keyup`, function(event) {
-    if (event.keyCode === 13) {
-     event.preventDefault();
-    }
-  });
-
 addField.addEventListener(`keyup`, function(e) {
-    console.log(e.keyCode);
     if (e.keyCode === 13) {
         e.preventDefault();
         addBtn.click();
@@ -35,19 +26,76 @@ function addItem(value) {
         let fieldset = document.createElement(`fieldset`);
         let checkbox = document.createElement(`input`);
         let label = document.createElement(`label`);
-        let span = document.createElement(`span`);
         let itemId = `item-${itemNum++}`;
+        let btnEdit = document.createElement(`button`);
+        let btnDel = document.createElement(`button`);
     
         checkbox.type = `checkbox`;
         checkbox.id = itemId;
+
+        btnEdit.classList.add(`material-icons`, `btn-edit`);
+        btnEdit.type = `button`;
+        btnEdit.innerHTML = `edit`;
+
+        btnDel.classList.add(`material-icons`, `btn-del`);
+        btnDel.type = `button`;
+        btnDel.innerHTML = `delete`;        
+
         label.setAttribute(`for`, itemId);
-        label.innerHTML = value + span.outerHTML;
+        label.innerHTML = value + btnEdit.outerHTML + btnDel.outerHTML;
+
         fieldset.innerHTML += checkbox.outerHTML + label.outerHTML;
     
         todoList.appendChild(fieldset);
         itemsAmount = todoList.getElementsByTagName(`fieldset`);
+        listenerItems();
     } else {
         console.log(`full`);
     }
 
 }
+
+function listenerItems() {
+    let btnsEdit = todoList.querySelectorAll(`.btn-edit`);
+    let btnsDel = todoList.querySelectorAll(`.btn-del`);
+    console.log(btnsDel);
+    for (let i = 0; i < btnsEdit.length; i++) {
+        btnsEdit[i].addEventListener(`click`, editItem, false);
+        btnsEdit[i].nextElementSibling.addEventListener(`click`, delItem, false);
+    }
+}
+
+function editItem() {
+    let inputText = document.createElement(`input`);
+    let btnSave = document.createElement(`button`);
+    let text = this.previousSibling.textContent;
+    let fieldset = this.parentElement.parentElement;
+    let label = this.parentElement;
+
+    btnSave.classList.add(`material-icons`, `btn-edit`);
+    btnSave.type = `button`;
+    btnSave.innerHTML = `save`;
+
+    inputText.type = `text`;
+    inputText.value = text;
+
+    fieldset.appendChild(inputText);
+    fieldset.appendChild(btnSave);
+    console.log(this.previousSibling.textContent);
+
+    btnSave.addEventListener(`click`, () => {
+        changeItem(inputText.value);
+    }, false);
+
+    function changeItem(value) {
+        console.log(label);
+        label.innerHTML = 'gg';
+        console.log(`done`);
+    }
+}
+
+function delItem() {
+    this.parentElement.parentElement.remove();
+}
+
+// onkeypress="return event.keyCode != 13"
